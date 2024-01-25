c21a970184777e48446874b03626356a5944431f
Sheep1sik <bohg6799@naver.com>
2024-01-25 23:43:17 +0900

    Feat: 뷰 생성 및 결합 Section 3까지

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 양원식 on 2024/01/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
